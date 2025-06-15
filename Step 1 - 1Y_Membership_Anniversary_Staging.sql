08624f808810d9033f4dea548f084a11997b810f
Dennis Chau <34014458+d7chau@users.noreply.github.com>
2025-06-14 22:45:48 -0400

    Updated Comments

--- Step 1 - 1Y_Membership_Anniversary_Staging.sql
+++ Step 1 - 1Y_Membership_Anniversary_Staging.sql
@@ -1,18 +1,24 @@
 /* SELECTING MEMBERS ELIGIBLE FOR 1-YEAR ANNIVERSARY CAMPAIGN */
 
 SELECT         MEMBER.*,
+  
+               /* TOTAL SPENT BY MEMBER IN THE LAST YEAR */
+               X.TotalSpent,
+  
                /* ASSIGN DISCOUNT TIER BASED ON MEMBER'S TOTAL SPEND IN THE LAST YEAR */
                CASE
-                    WHEN X.TotalSpent > 0 AND X.TotalSpent <= 500 THEN '10%'
-                    WHEN X.TotalSpent > 500 AND X.TotalSpent <= 1000 THEN '15%'
-                    ELSE '20%'
+                    WHEN X.TotalSpent > 0 AND X.TotalSpent <= 500 THEN '10%'  -- Low Spenders
+                    WHEN X.TotalSpent > 500 AND X.TotalSpent <= 1000 THEN '15%'  -- Mid Spenders
+                    ELSE '20%'  -- High Spenders
                END AS MembershipDiscount,
-               X.TotalSpent,
+  
                /* SETTING DISCOUNT EXPIRATION TO 1 MONTH FROM TODAY */
                DATEADD(MONTH, 1, GETDATE() AT TIME ZONE 'Central Standard Time' AT TIME ZONE 'Eastern Standard Time') AS DiscountExpiration,
+  
                /* CAPTURE CURRENT TIMESTAMP FOR JOURNEY ENTRY */
                GETDATE() AT TIME ZONE 'Central Standard Time' AT TIME ZONE 'Eastern Standard Time' AS JourneyEntryDate
 
+/* MASTER LIST OF ALL MEMBERS WITH PROFILE DATA */
 FROM           [Membership_Master_DE] MEMBER
 
 /* CALCULATION OF TOTAL SPENT WITHIN LAST YEAR */
@@ -46,4 +52,5 @@ AND            NOT EXISTS (
                               SELECT         1
                               FROM           [1Y_Membership_Anniversary_JourneyEntry] JOURNEY
                               WHERE          MEMBER.MemberID = JOURNEY.MemberID
+                              /* JOURNEY ENTRY EXISTS -> ALREADY PROCESSED */
                           )
