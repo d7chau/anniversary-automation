f21bfd25fa91fcf1167513381636b0729f8b362b
Dennis Chau <34014458+d7chau@users.noreply.github.com>
2025-06-17 15:31:55 -0400

    Updated Comment

--- Step 3 - 1Y_Membership_Anniversary_JourneyEntry - Update Flags.sql
+++ Step 3 - 1Y_Membership_Anniversary_JourneyEntry - Update Flags.sql
@@ -4,6 +4,6 @@ SELECT          JOURNEY.MemberID,
                 1 AS HasClaimedDiscount -- Flag Indicating Member Has Redeemed Discount (1 = True)
 FROM            [1Y_Membership_Anniversary_JourneyEntry] JOURNEY
 
-/* JOINING WITH REDEMPTION LOG TO IDENTIFY MEMBERS WHO HAVE REDEEMED */
+/* JOINING WITH REDEMPTION LOG TO IDENTIFY ONLY MEMBERS WHO HAVE REDEEMED */
 INNER JOIN      [Redemption_Log_DE] REDEEM -- Members Who Have Already Redeemed Discount
 ON              JOURNEY.MemberID = REDEEM.MemberID
